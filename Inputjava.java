021f6037c2d5096c50b1512e5d44ff606447f05f
Mohit Gehlot <93043678+mohitgehlot20@users.noreply.github.com>
2022-09-14 12:17:10 +0530

    Add files via upload

---
+++ Inputjava.java
@@ -0,0 +1,30 @@
+package com.edu.input;
+import java.util.*;
+
+
+public class Inputjava {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Hello World");
+//		
+		 public static void main(String args[])
+		    {
+		        // Using Scanner for Getting Input from User
+		        Scanner in = new Scanner(System.in);
+		 
+		        String s = in.nextLine();
+		        System.out.println("You entered string " + s);
+		 
+		        int a = in.nextInt();
+		        System.out.println("You entered integer " + a);
+		 
+		        float b = in.nextFloat();
+		        System.out.println("You entered float " + b);
+		    }
+		
+
+	
+	}
+
+
